3995bc4101b531d688f7ccaf89721fdd3d064b87
dcvela23 <dcvela23@outlook.es>
2017-03-06 16:25:20 +0000

    comic_charts_almost_ok

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -17,6 +17,11 @@ function makeGraphs(error, projectsJson){
     var comicDim = ndx.dimension( function (d) {
         return d["Comic"];
     });
+
+    var hairDim = ndx.dimension( function (d) {
+        return d["HAIR"];
+    })
+
     var nameDim = ndx.dimension( function (d) {
        return d["name"];
     });
@@ -36,46 +41,125 @@ function makeGraphs(error, projectsJson){
         return d["YEAR"];
     });
 
+    var genderDim = ndx.dimension(function (d) {
+        return d["SEX"];
+    });
+
+    var eyeDim = ndx.dimension(function (d) {
+        return d['EYE']
+    });
+
+    var orientation = ndx.dimension(function (d) {
+        return d['GSM']
+    });
 
 // METRICS
 
-    var total_of_characters = comicDim.groupAll().reduceCount(function (d) {
-        return d["Comic"];
+    var total_of_characters = nameDim.groupAll().reduceCount(function (d) {
+        return d["name"]
     });
 
-    var totalComic = comicDim.group();
+    var total_Comic = comicDim.group();
+    var total_name = nameDim.group();
+    var total_id = IdDim.group();
+    var total_align = alignDim.group();
+    var total_alive = aliveDim.group();
+    var total_appearance = nameDim.group().reduceSum(function (d) {
+            return d["APPEARANCES"];
+        }
+    );
+    var total_year = yearDim.group();
+    var total_gender = genderDim.group();
+    var total_hair = hairDim.group();
+    var total_eye = eyeDim.group();
 
-    var totalAlign = alignDim.group().reduceCount(function (d) {
-        return d['ALIGN']
-    });
+// MAX AND MIN YEAR
+
+    var minYear = yearDim.bottom(1)[0]["YEAR"];
+    var maxYear = yearDim.top(1)[0]["YEAR"];
+
+ // SELECT MENUS
+    selectField = dc.selectMenu("#select_comic")
+        .dimension(comicDim)
+        .group(total_Comic);
+
+    selectField = dc.selectMenu("#select_gender")
+        .dimension(genderDim)
+        .group(total_gender);
+
+    selectField = dc.selectMenu("#select_hair")
+        .dimension(hairDim)
+        .group(total_hair);
+
+    selectField = dc.selectMenu("#select_eye")
+        .dimension(eyeDim)
+        .group(total_eye);
 
 // CHARTS
 
-
+     var totalIdChart = dc.rowChart('#identity');
+     var totalAliveChart = dc.pieChart('#dead_or_alive');
      var totalCharactersChart = dc.numberDisplay("#total_characters");
      var totalAlignChart = dc.pieChart('#total_align');
+     var totalYearChart = dc.lineChart('#year_appearance');
+     var rankingNameChart = dc.rowChart('#ranking_names');
+
+
+
 
  // STYLING CHARTS
 
-selectField = dc.selectMenu("#select_comic")
-    .dimension(comicDim)
-    .group(totalComic)
+
+    rankingNameChart
+        .width(300)
+        .height(300)
+        .dimension(nameDim)
+        .group(total_appearance);
+    rankingNameChart.ordering(function (d) { return -d.value});
+    rankingNameChart.rowsCap([7]);
+    rankingNameChart.othersGrouper(false);
 
 
-totalCharactersChart
-    .formatNumber(d3.format("d"))
-    .valueAccessor(function (d) {
-        return d;
-    })
-    .group(total_of_characters);
+    totalIdChart
+        .width(300)
+        .height(150)
+        .transitionDuration(1500)
+        .dimension(IdDim)
+        .group(total_id);
 
-totalAlignChart
-    .width(300)
-    .height(150)
-    .innerRadius(20)
-    .transitionDuration(1500)
-    .dimension(alignDim)
-    .group(totalAlign);
+    totalAliveChart
+        .width(300)
+        .height(150)
+        .innerRadius(20)
+        .transitionDuration(1500)
+        .dimension(aliveDim)
+        .group(total_alive);
+
+    totalCharactersChart
+        .formatNumber(d3.format("d"))
+        .valueAccessor(function (d) {
+            return d;
+        })
+        .group(total_of_characters);
+
+    totalAlignChart
+        .width(300)
+        .height(150)
+        .innerRadius(20)
+        .transitionDuration(1500)
+        .dimension(alignDim)
+        .group(total_align);
+
+    totalYearChart
+        .width(1000)
+        .height(200)
+        .dimension(yearDim)
+        .group(total_year)
+        .x(d3.scale.ordinal().domain([(minYear),(maxYear)]).rangeBands([0,10]))
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .elasticX(true)
+        .brushOn(false);
 
 dc.renderAll();
 };
